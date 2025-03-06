a3a0ac245b8bb903adca580fa5a7523070b18d8d
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-05 21:43:13 -0300

    Builds client-form & client-table components

--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -3,10 +3,11 @@ import { Component, Inject } from '@angular/core';
 import { IClientService } from '../../services/api-client/clients/iclients.service';
 import { ClientsService } from '../../services/api-client/clients/clients.service';
 import { SERVICES_TOKEN } from '../../services/service.token';
+import { ClientFormComponent } from "../components/client-form/client-form.component";
 
 @Component({
     selector: 'app-new-client',
-    imports: [],
+    imports: [ClientFormComponent],
     templateUrl: './new-client.component.html',
     styleUrl: './new-client.component.scss',
     providers: [
