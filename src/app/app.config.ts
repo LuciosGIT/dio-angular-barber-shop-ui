f75784a35c70fe47cf86298dcd6818cce884eb47
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-05 21:55:01 -0300

    Project built

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,17 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-import { provideNgxMask } from 'ngx-mask';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true}),
-     provideRouter(routes),
-     provideAnimationsAsync(),
-     provideHttpClient(withInterceptorsFromDi()),
-     provideNgxMask({})
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideNgxMask({})
   ]
 };
-
-
