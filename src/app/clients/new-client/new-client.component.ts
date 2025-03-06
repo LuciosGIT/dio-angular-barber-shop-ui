0820468e1ac04de0dc08c8e09aea68be0c91909d
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-05 21:46:24 -0300

    List-clients & card-header components build

--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,25 +1,47 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { SERVICES_TOKEN } from '../../services/service.token';
 import { IClientService } from '../../services/api-client/clients/iclients.service';
 import { ClientsService } from '../../services/api-client/clients/clients.service';
-import { SERVICES_TOKEN } from '../../services/service.token';
-import { ClientFormComponent } from "../components/client-form/client-form.component";
+import { ClientModelForm } from '../client.models';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { SnackbarManagerService } from '../../services/snackbar-manager.service';
+import { ISnackbarManagerService } from '../../services/isnackbar-manager.service';
+import { ClientFormComponent } from '../components/client-form/client-form.component';
 
 @Component({
-    selector: 'app-new-client',
-    imports: [ClientFormComponent],
-    templateUrl: './new-client.component.html',
-    styleUrl: './new-client.component.scss',
-    providers: [
-    {
-      provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService
-    }
-    ]
+  selector: 'app-new-client',
+  imports: [ClientFormComponent],
+  templateUrl: './new-client.component.html',
+  styleUrl: './new-client.component.scss',
+  providers: [
+    { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService },
+    { provide: SERVICES_TOKEN.SNACKBAR, useClass: SnackbarManagerService }
+  ]
 })
-export class NewClientComponent {
+export class NewClientComponent implements OnDestroy {
 
-  constructor(@Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService) {
+  private httpSubscription?: Subscription
 
+  constructor(
+    @Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService,
+    @Inject(SERVICES_TOKEN.SNACKBAR) private readonly snackBarManager: ISnackbarManagerService,
+    private readonly router: Router
+  ) { }
+
+  ngOnDestroy(): void {
+    if (this.httpSubscription) {
+      this.httpSubscription.unsubscribe()
+    }
   }
+
+  onSubmitClient(value: ClientModelForm) {
+    const { id, ...request } = value
+    this.httpSubscription = this.httpService.save(request).subscribe(_ => {
+      this.snackBarManager.show('Usuário cadastrado com sucesso')
+      this.router.navigate(['clients/list'])
+    })
+  }
+
 
 }
